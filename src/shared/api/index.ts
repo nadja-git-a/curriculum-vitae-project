e6588207d11ed2e322c6c3c306faa6c5d6d541d9
nadja-git-a <nadja.smartx4@gmail.com>
2025-11-19 18:13:49 +0300

    put url in .env

--- src/shared/api/index.ts
+++ src/shared/api/index.ts
@@ -2,7 +2,7 @@ import axios, { AxiosError, type AxiosRequestConfig } from "axios";
 
 import { useAuthStore } from "app/store/authStore";
 
-export const API_URL = "https://cv-project-js.inno.ws/api/graphql";
+export const API_URL = import.meta.env.VITE_API_URL;
 
 export const api = axios.create({
   baseURL: API_URL,
